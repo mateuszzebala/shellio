110ce69d075f2b87ff379f940fe16d33d9ae5ccf
mateuszzebala <mateusz.zebala.pl@gmail.com>
2025-05-20 20:27:18 +0200

    change version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="shellio",
-    version="0.0.1",
+    version="0.0.2",
     author="Mateusz Zębala",
     author_email="mateusz.zebala.pl@gmail.com",
     description="ShellIO",
